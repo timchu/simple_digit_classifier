fixed up backprop, though errors remain. See current_state for my thoughts.

double_layer_nn.py
@@ -2,39 +2,30 @@ import numpy as np
 import feedforward as ff
 import backprop as bp
 import math
+import initial_value_fns as init
+import scrape_data as sd
 
-# returns 
+"""" 784, 100, 10 neural network on one example."""
 
-#Discovering dLoss_dLin(k) after we've calculated relevant neurons (and linear pieces underlying said neurons) 
-#Requires having a neural network architecture;
+#Initiailzation of Hyperparameters
+input_size = 784
+l1_size = 100 #layer_1 size
+l2_size = 10
 
-# lin3 = the linear variables underlying each neuron on layer 3.
-# layer3 = value of each neuron at said layer.
-# Each dLoss_dx is a column vector of size len(x) by 1.
+# Initialiation of parameters
+syn0 = np.array(init.RandomWeights(input_size, l1_size))
+syn1 = np.array(init.RandomWeights(l1_size, l2_size))
+epochs = 100
+learning_rate = 0.1
 
-# This is a single pass iteration with a single example. layer2 is a single matrix
-
-# TEST: does this code actually  work?
-# No biases added in yet... that's not good! Add the 1 to the x. Calculation of linear things is done through feed-forward.
-
-"""" 3, 4, 2 neural network on one example."""
-
-#Initiailzation of weights
-syn0 = np.array([[1, -1, -1, 1], [1, -1, -1, 0.4], [1, -0.2, 0.3, 0.4], [1, -0.2, -1, 1]]) #last row is the biases.
-syn1 = np.array([[0.1,0.2],[0.3,-0.4],[-0.5,0.6],[0.7,-0.8],[-1, 1]])
-
-# Data from 1 example
-X = np.array([1, 1, 1])
-y = np.array([0,1])
-
-syns = [syn0, syn1]
-
-for i in range(20000):
-  (lins, layers) = ff.feedforward(X, [syn0, syn1], ["sigmoid", "softmax"])
-  steps =  bp.one_bp_step(0.1, X, y, lins, layers, syns)
-  syn0 += steps[0]
-  syn1 += steps[1]
-
-print syn0
-print syn1
-print layers
+data = sd.getData("digitstrain.txt")[::100] #set this back to the whole dataset
+for i in range(epochs):
+    syns = [syn0, syn1]
+    for input_val in data:
+        (X, y) = init.getXY(input_val)
+        (lins, layers) = ff.feedforward(X, syns, ["sigmoid", "softmax"])
+        steps =  bp.one_bp_step(learning_rate, X, y, lins, layers, syns)
+        syn0 += steps[0]#/num_samples. 
+        syn1 += steps[1]#/num_samples
+        if i  == 99:
+            print layers[2]

feedforward.py
@@ -9,7 +9,7 @@ def ff1(l0, syn, neural_type="sigmoid"):
     lin1 = dt(l, syn)
     return (lin1, nonlin(neural_type, lin1))
 
-def  printme(a, name="MARKER"):
+def printme(a, name="MARKER"):
     print name
     print a
 
@@ -18,12 +18,11 @@ def  printme(a, name="MARKER"):
 def feedforward(x0, syns, neural_types_list):
     lins = [x0]
     layers = [x0]
-    cur_layer = np.array([x0])
-    v = ap(x0, 1) # to account for bias, the last row of syn
+    cur_layer = x0
     for i in xrange(len(syns)):
-        neural_type = neural_types_list[i]
+        nt = neural_types_list[i]
         syn = syns[i]
-        (next_layer_lin, next_layer) = ff1(cur_layer, syn)
+        (next_layer_lin, next_layer) = ff1(cur_layer, syn, nt)
 
         # Post loop updating
         # set layers 

neural_fns.py
@@ -40,10 +40,8 @@ def softmax(x_layer):
     sum_exp = 0
     softmax = np.zeros(len(x_layer))
     for x in x_layer:
-        sum_exp += math.exp(x)
-    for i in xrange(len(softmax)):
-        softmax[i] = math.exp(x_layer[i])/sum_exp
-    return softmax
+      sum_exp += math.exp(x)
+    return [math.exp(x)/sum_exp for x in x_layer]
 
 # Utility functions to calculate the derivative. Can be put into own file.
 